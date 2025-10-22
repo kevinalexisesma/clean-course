46eeeaa39d94e4565392f9c8271e1beb6f59e093
kevinalexisesma <81497048+kevinalexisesma@users.noreply.github.com>
2025-10-22 06:28:20 -0500

    Refactor(Functions): refactoring function

--- src/clean-code/03-functions.ts
+++ src/clean-code/03-functions.ts
@@ -40,23 +40,12 @@
     }
 
     // Continuar
-      const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false }) => {
-        let result;
-        if ( isDead ) {
-            result = 1500;
-        } else {
-            if ( isSeparated ) {
-                result = 2500;
-            } else {
-                if ( isRetired ) {
-                    result = 3000;
-                } else {
-                    result = 4000; 
-                }
-            }
-        }
-        
-        return result;
+      const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false }):number => {        
+        if ( isDead ) return 1500;
+
+        if ( isSeparated ) return 2500;                
+
+        return isRetired ? 3000 : 4000;        
     }
 
 })();
