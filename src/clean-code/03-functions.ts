f21017548be0c0d032c2352ba7e9afaf3fb7d75a
kevinalexisesma <81497048+kevinalexisesma@users.noreply.github.com>
2025-10-22 06:22:16 -0500

    Feat(Functions): adding function to refactor

--- src/clean-code/03-functions.ts
+++ src/clean-code/03-functions.ts
@@ -38,4 +38,25 @@
         return true;        
 
     }
+
+    // Continuar
+      const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false }) => {
+        let result;
+        if ( isDead ) {
+            result = 1500;
+        } else {
+            if ( isSeparated ) {
+                result = 2500;
+            } else {
+                if ( isRetired ) {
+                    result = 3000;
+                } else {
+                    result = 4000; 
+                }
+            }
+        }
+        
+        return result;
+    }
+
 })();
