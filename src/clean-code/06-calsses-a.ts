58110e64a8d1b7d81ab3b11e223a714aa84ec2a7
kevinalexisesma <81497048+kevinalexisesma@users.noreply.github.com>
2025-10-23 16:07:59 +0000

    Feat(problematic inheritance): improving  base file

--- src/clean-code/06-calsses-a.ts
+++ src/clean-code/06-calsses-a.ts
@@ -12,7 +12,54 @@
         ){}
     }
 
-    const newPerson = new Person('Fernando', 'M', new Date('1985-10-21'));
-    console.log({ newPerson });
+
+    class User extends Person {
+        
+        public lastAccess: Date;
+
+        constructor(
+            public email: string,
+            public role: string,
+            name: string,
+            gender: Gender,
+            birthdate: Date,
+        ) {
+            super( name, gender, birthdate );
+            this.lastAccess = new Date();
+        }
+
+        checkCredentials() {
+            return true;
+        }
+    }
+
+
+    class UserSettings extends User {
+        constructor(
+            public workingDirectory: string,
+            public lastOpenFolder  : string,
+            email                  : string,
+            role                   : string,
+            name                   : string,
+            gender                 : Gender,
+            birthdate              : Date
+        ) {
+            super(email, role, name, gender, birthdate );
+        }
+    }
+
+
+    const userSettings = new UserSettings(
+        '/usr/home',
+        '/home',
+        'fernando@google.com',
+        'Admin',
+        'Fernando',
+        'M',
+        new Date('1985-10-21')
+    );
+
+    console.log({ userSettings });
+
 
 })();
