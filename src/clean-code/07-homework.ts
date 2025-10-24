175db039a54a420dbc09abe9afac357aeff32868
Kevin Eslava <kevinalexisesma@ufps.edu.co>
2025-10-23 22:05:32 -0500

    Refactor(SOLID): applying the principle of single responsibility to the task

--- src/clean-code/07-homework.ts
+++ src/clean-code/07-homework.ts
@@ -12,31 +12,38 @@
         ) {}
     }
 
-    class InputAttributes extends HtmlElement {
+    class InputAttributes {
         constructor(
             public value: string,
-            public placeholder: string,
-            id: string,
-        ) {
-            super(id, 'input');
+            public placeholder: string,            
+        ) {            
         }
     }
 
-    class InputEvents extends InputAttributes {
-        constructor( value: string, placeholder: string, id: string ) {
-            super( value, placeholder, id );
+    class InputEvents {        
+        constructor() {            
         }
-
         setFocus() {};
         getValue() {};
         isActive() {};
         removeValue() {};
     }
 
+    class InputElement {        
+        public html: HtmlElement;
+        public attributes: InputAttributes;
+        public events: InputEvents;
+        constructor( value: string, placeholder: string, id: string ) {
+            this.html = new HtmlElement(id, 'input');
+            this.attributes = new InputAttributes(value, placeholder);
+            this.events = new InputEvents();
+        }
+    }
+
 
     //? Idea para la nueva clase InputElement
 
-    const nameField = new InputEvents('Fernando', 'Enter first name', 'txtName');
+    const nameField = new InputElement('Fernando', 'Enter first name', 'txtName');
 
     console.log({ nameField });
 
