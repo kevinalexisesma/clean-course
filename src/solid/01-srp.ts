69acc49bcc6923f5696488b2922b3e76d01e5cf9
Kevin Eslava <kevinalexisesma@ufps.edu.co>
2025-10-26 17:48:32 -0500

    Refactor(SOLID): applying the principle of single responsibility

--- src/solid/01-srp.ts
+++ src/solid/01-srp.ts
@@ -4,12 +4,9 @@
         id:   number;
         name: string;
     }
-    
-    // Usualmente, esto es una clase para controlar la vista que es desplegada al usuario
-    // Recuerden que podemos tener muchas vistas que realicen este mismo trabajo.
-    class ProductBloc {
-    
-        loadProduct( id: number ) {
+
+    class ProductService {
+        getProductById( id: number ) {
             // Realiza un proceso para obtener el producto y retornarlo
             console.log('Producto: ',{ id, name: 'OLED Tv' });
         }
@@ -18,26 +15,60 @@
             // Realiza una petición para salvar en base de datos 
             console.log('Guardando en base de datos', product );
         }
+    }
+
+    class Mailer{
+        private masterEmail: string = 'admin@tienda.com';
+
+        sendEmail( emailList: string[], template: 'to-clients' | 'to-admin' ) {
+            console.log('Enviando correo a los clientes',template);
+        }
+    }
+    
+    class ProductBloc {
+
+        private productService: ProductService;
+        private mailer: Mailer; 
+
+        constructor( productService: ProductService, mailer: Mailer ) {
+            this.productService = productService;
+            this.mailer = mailer;
+        }
+    
+        loadProduct( id: number ) {
+            this.productService.getProductById( id );
+        }
+    
+        saveProduct( product: Product ) {
+            this.productService.saveProduct( product );
+        }
     
         notifyClients() {
-            console.log('Enviando correo a los clientes');
-        }
+            this.mailer.sendEmail(['kevin@google.com'], 'to-clients');
+        }        
     
-        onAddToCart( productId: number ) {
-            // Agregar al carrito de compras
+    }
+
+    class CartBloc {
+
+        private produtsInCart: Object[] = [];
+
+        onAddToCart( productId: number ) {            
             console.log('Agregando al carrito ', productId );
         }
-    
     }
     
 
+    const productService = new ProductService();
+    const mailer = new Mailer();
 
-    const productBloc = new ProductBloc();
+    const productBloc = new ProductBloc(productService, mailer);
+    const cartBloc = new CartBloc();
 
     productBloc.loadProduct(10);
     productBloc.saveProduct({ id: 10, name: 'OLED TV' });
     productBloc.notifyClients();
-    productBloc.onAddToCart(10);
+    cartBloc.onAddToCart(10);
 
 
 
