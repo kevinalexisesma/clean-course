14f4541bc73c6fb04980dc533a9757f942c76e0c
Kevin Eslava <kevinalexisesma@ufps.edu.co>
2025-10-26 20:02:21 -0500

    Refactor(SOLID): applying the principle of open and close

--- src/solid/02-open-close-b.ts
+++ src/solid/02-open-close-b.ts
@@ -1,10 +1,11 @@
-import axios from 'axios';
-
+import { HttpClient } from "./02-open-close-c";
 
 export class TodoService { 
 
+    constructor( private httpClient: HttpClient) {}
+
     async getTodoItems() {
-        const { data } = await axios.get('https://jsonplaceholder.typicode.com/todos/');
+        const { data } = await this.httpClient.get('https://jsonplaceholder.typicode.com/todos/');
         return data;
     }
 }
@@ -12,8 +13,10 @@ export class TodoService {
 
 export class PostService {
 
+    constructor( private httpClient: HttpClient) {}
+
     async getPosts() {
-        const { data } = await axios.get('https://jsonplaceholder.typicode.com/posts');
+        const { data } = await this.httpClient.get('https://jsonplaceholder.typicode.com/posts');
         return data;
     }
 }
@@ -21,8 +24,10 @@ export class PostService {
 
 export class PhotosService {
 
+constructor( private httpClient: HttpClient) {}
+
     async getPhotos() {
-        const { data } = await axios.get('https://jsonplaceholder.typicode.com/photos');
+        const { data } = await this.httpClient.get('https://jsonplaceholder.typicode.com/photos');
         return data;
     }
 
