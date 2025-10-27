c58d0f55ef2b8d252a3afb211674c0f8273a1c24
Kevin Eslava <kevinalexisesma@ufps.edu.co>
2025-10-27 07:03:08 -0500

    Refactor(SOLID): applying the principle of interface segregation

--- src/solid/04-segregation.ts
+++ src/solid/04-segregation.ts
@@ -1,35 +1,35 @@
-interface Bird{
-    fly(): void;
-    eat(): void;
+interface Bird{    
+    eat(): void;    
+}
+
+interface FlyingBird extends Bird{
+    fly(): number;
+}
+
+interface runningBird extends Bird{
     run(): void;
+}
+
+interface SwimmingBird extends Bird{
     swim(): void;
 }
 
-
-class Tucan implements Bird{
-    fly(): void {}
-    eat(): void {}
-    run(): void {}
-    swim(): void {}
+class Tucan implements Bird, FlyingBird{
+    fly(): number { return 100;}
+    eat(): void {}        
 }
 
-class Hummingbird implements Bird{
-    fly(): void {}
-    eat(): void {}
-    run(): void {}
-    swim(): void {}
+class Hummingbird implements Bird, FlyingBird{
+    fly(): number { return 200;}
+    eat(): void {}        
 }
 
-class Ostrich implements Bird{
-    fly(): void {}
+class Ostrich implements Bird, runningBird{    
     eat(): void {}
-    run(): void {}
-    swim(): void {}
+    run(): void {}    
 }
 
-class Penguin implements Bird{
-    fly(): void {}
-    eat(): void {}
-    run(): void {}
+class Penguin implements Bird, SwimmingBird{    
+    eat(): void {}    
     swim(): void {}
 }
