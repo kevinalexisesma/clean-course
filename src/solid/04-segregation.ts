4c6fd2f76c9ee0761700aa3e23e9956bfdbd5f00
Kevin Eslava <kevinalexisesma@ufps.edu.co>
2025-10-27 06:59:13 -0500

    Feat(SOLID): Interface segregation

---
+++ src/solid/04-segregation.ts
@@ -0,0 +1,35 @@
+interface Bird{
+    fly(): void;
+    eat(): void;
+    run(): void;
+    swim(): void;
+}
+
+
+class Tucan implements Bird{
+    fly(): void {}
+    eat(): void {}
+    run(): void {}
+    swim(): void {}
+}
+
+class Hummingbird implements Bird{
+    fly(): void {}
+    eat(): void {}
+    run(): void {}
+    swim(): void {}
+}
+
+class Ostrich implements Bird{
+    fly(): void {}
+    eat(): void {}
+    run(): void {}
+    swim(): void {}
+}
+
+class Penguin implements Bird{
+    fly(): void {}
+    eat(): void {}
+    run(): void {}
+    swim(): void {}
+}
